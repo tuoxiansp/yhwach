4037065d5c1a528b423b42b77863711fe61bfb45
ChenYutong <b1ncer@foxmail.com>
2017-02-27 19:21:00 +0800

    fix shallowCompare bug.

--- shallowCompare.js
+++ shallowCompare.js
@@ -1,5 +1,5 @@
 import shallowEqualIgnoreFunction from './shallowEqualIgnoreFunction'
 
 export default function shallowCompare(instance, nextProps, nextState) {
-    return !shallowEqualIgnoreFunction(instance.props, nextProps) || shallowEqualIgnoreFunction(instance.state, nextState)
+    return !shallowEqualIgnoreFunction(instance.props, nextProps) || !shallowEqualIgnoreFunction(instance.state, nextState)
 }
